d8cbd67b85a4e02f4a055d392f0f6052fe2fbc17
cutefluffyfox <pinkaiscrazy@gmail.com>
2022-03-04 16:56:23 +0300

    Ex1 attempt 3

--- ex01.hpp
+++ ex01.hpp
@@ -2,7 +2,7 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-template <typename T>
+template <typename T = int>
 void multiples (T& sum, T x, T n){
     for (int i = 1; i <= n; i++){
         sum += i*x;
