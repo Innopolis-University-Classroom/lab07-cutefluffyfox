fd1c9d6c0285b3a442993adfc383b4179b8ae97c
cutefluffyfox <pinkaiscrazy@gmail.com>
2022-03-04 17:13:28 +0300

    idk what's wrong with my solution anymore

--- ex01.hpp
+++ ex01.hpp
@@ -2,7 +2,7 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-template <typename T = int>
+template<typename T = int>
 void multiples (T& sum, T x, int n){
     sum += 1;
     for (int i = 1; i <= n; i++){
