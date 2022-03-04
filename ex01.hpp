4a81cf3648c7bc127aea8b4cea786b3d655ac1d7
cutefluffyfox <pinkaiscrazy@gmail.com>
2022-03-04 17:23:04 +0300

    Finally

--- ex01.hpp
+++ ex01.hpp
@@ -1,9 +1,5 @@
-
-//change the below code
-// the function below now only works with integers, change it to work with any type
-// also fill out the body
 template<typename T = int>
-void multiples (T& sum, T x, int n){
+void multiples(T& sum, T x, int n){
     sum += 1;
     for (int i = 1; i <= n; i++){
         sum += i*x;
